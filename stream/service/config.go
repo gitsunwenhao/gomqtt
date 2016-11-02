2ae70641d210cff75ab5eb8e7422ceb1930690aa
shaocc <kugou2008.happy@163.com>
2016-11-02 15:22:55 +0800

    add stream load to apns

---
+++ stream/service/config.go
@@ -0,0 +1,50 @@
+package service
+
+import (
+	"io/ioutil"
+	"log"
+
+	"fmt"
+
+	"github.com/naoina/toml"
+	"github.com/uber-go/zap"
+)
+
+type Config struct {
+	Common struct {
+		Version  string
+		IsDebug  bool
+		LogLevel string
+		LogPath  string
+	}
+}
+
+var Conf = &Config{}
+
+func loadConfig(staticConf bool) {
+	var contents []byte
+	var err error
+
+	if staticConf {
+		//静态配置
+		contents, err = ioutil.ReadFile("configs/stream.toml")
+	} else {
+		contents, err = ioutil.ReadFile("/etc/gomqtt/stream.toml")
+	}
+
+	if err != nil {
+		log.Fatal("load config error", zap.Error(err))
+	}
+
+	tbl, err := toml.Parse(contents)
+	if err != nil {
+		log.Fatal("parse config error", zap.Error(err))
+	}
+
+	toml.UnmarshalTable(tbl, Conf)
+
+	fmt.Println(Conf)
+
+	// 初始化Logger
+	InitLogger(Conf.Common.LogPath, Conf.Common.LogLevel, Conf.Common.IsDebug)
+}
